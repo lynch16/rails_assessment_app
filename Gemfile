70af049304508500290cfc9130466f81db1edea9
lynch16 <will.lynch91@gmail.com>
2016-11-27 19:15:00 -0500

    devise added with omniauth

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'pry'
 end
 
 group :development do
@@ -44,6 +45,8 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+#Devise Authorization
+gem 'devise'
 #OmniAuth Gmail Integration
 gem 'gmail'
 gem 'omniauth'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'sessions#destroy', as: 'signout'
 
-  resources :sessions, only: [:create, :destroy]
-
-  root to: 'sessions#new'
+  root to: 'users#index'
+  resources :users, except: [:destroy]
 end
